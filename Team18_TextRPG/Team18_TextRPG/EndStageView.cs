2ade75dbc4185db9b46f3043f4a96d62e0258471
NileBlue0210 <bjw8594@gmail.com>
2025-04-22 17:05:41 +0900

    EndStageView의 Player클래스 관련 오류 수정

--- Team18_TextRPG/Team18_TextRPG/EndStageView.cs
+++ Team18_TextRPG/Team18_TextRPG/EndStageView.cs
@@ -2,20 +2,22 @@
 
 namespace EndStageView
 {
-    Player player = new Player();
     internal class EndStageView
     {
         static void EndStage()
         {
+            Player player = new Player();
+            ConvertClassCode convertClassCode = new ConvertClassCode();
+
             Console.WriteLine("배틀 결과\n");
             Console.WriteLine(""); //승리 & 패배, 시간차 주기
             Console.WriteLine("system message\n");
             Console.WriteLine("+==================+");
 
             Console.WriteLine("내 정보\n");
-            Console.WriteLine($"{player.Name}({player.Classcode})");
-            Console.WriteLine($체력: {hp}""); //HP 변화량 보여주기?
-            Console.WriteLine($"골드: {gold}\n\n"); //골드 변화량 보여주기?
+            Console.WriteLine($"{player.Name}({convertClassCode.ConvertClassCodeToString(player.ClassCode)})");
+            Console.WriteLine($"체력: {player.Health}"); //HP 변화량 보여주기?
+            Console.WriteLine($"골드: {player.Gold}\n\n"); //골드 변화량 보여주기?
             Console.WriteLine("대상을 선택해주세요>>\n");
             Console.WriteLine("전투옵션\n");
             Console.WriteLine("0.다음");
@@ -30,5 +32,6 @@ namespace EndStageView
                     Console.WriteLine("잘못된 입력입니다..");
                     continue;
             }
+        }
     }
 }

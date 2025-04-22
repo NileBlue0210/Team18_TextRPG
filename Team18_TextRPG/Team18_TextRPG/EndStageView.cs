56cc24923cf5ee2bd8a498b5791e76b99298c9c7
Saint_Elion <pocklyuck@naver.com>
2025-04-22 20:09:31 +0900

    1. MainMenu 연결 2. while문 추가

--- Team18_TextRPG/Team18_TextRPG/EndStageView.cs
+++ Team18_TextRPG/Team18_TextRPG/EndStageView.cs
@@ -1,39 +1,41 @@
 ﻿using Sparta_Team18_TextRPG;
 
-namespace EndStageView
+namespace Sparta_Team18_TextRPG
 {
     internal class EndStageView
     {
         static void EndStage()
         {
-            Player player = new Player();
-            ConvertClassCode convertClassCode = new ConvertClassCode();
-
-            Console.WriteLine("배틀 결과\n");
-            Console.WriteLine(""); //승리 & 패배, 시간차 주기
-            Console.WriteLine("system message\n");
-            Console.WriteLine("+==================+");
-
-            Console.WriteLine("내 정보\n");
-            Console.WriteLine($"{player.Name}({convertClassCode.ConvertClassCodeToString(player.ClassCode)})");
-            Console.WriteLine($"체력: {player.Health}"); //HP 변화량 보여주기?
-            Console.WriteLine($"골드: {player.Gold}\n\n"); //골드 변화량 보여주기?
-            Console.WriteLine("대상을 선택해주세요>>\n");
-            Console.WriteLine("전투옵션\n");
-            Console.WriteLine("0.다음");
-
-            string input = Console.ReadLine();
-            /*
-            switch (input)
+            while (true)
             {
-                case "0":
-                    // 다음 스테이지로 이동하는 코드
-                    break;
-                default:
-                    Console.WriteLine("잘못된 입력입니다..");
-                    continue;
+                Player player = new Player();
+                ConvertClassCode convertClassCode = new ConvertClassCode();
+
+                Console.WriteLine("배틀 결과\n");
+                Console.WriteLine(""); //승리 & 패배, 시간차 주기
+                Console.WriteLine("system message\n");
+                Console.WriteLine("+==================+");
+
+                Console.WriteLine("내 정보\n");
+                Console.WriteLine($"{player.Name}({convertClassCode.ConvertClassCodeToString(player.ClassCode)})");
+                Console.WriteLine($"체력: {player.Health}");
+                Console.WriteLine($"골드: {player.Gold}\n\n");
+                Console.WriteLine("대상을 선택해주세요>>\n");
+                Console.WriteLine("전투옵션\n");
+                Console.WriteLine("0.다음");
+
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        MainMenu main = new MainMenu();
+                        main.DisplayMainMenu();
+                        return;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다..");
+                        continue;
+                }
             }
-            */
         }
     }
 }

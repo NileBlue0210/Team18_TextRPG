de41912d1c86aacd2e28b0935f12ddcb2adcaca5
wallyyoo <hyase22@naver.com>
2025-04-22 20:22:26 +0900

    sangwon : stat 창에서 나가기 로직

--- Team18_TextRPG/Team18_TextRPG/Status.cs
+++ Team18_TextRPG/Team18_TextRPG/Status.cs
@@ -3,37 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Sparta_Team18_TextRPG; //Player클래스 네임스페이스
+using Sparta_Team18_TextRPG; 
 
 namespace Sparta_Team18_TextRPG
 {
     class Status
     {
+        Player player = new Player();
+        ConvertClassCode convertClassCode = new ConvertClassCode();
+        MainMenu mainMenu = new MainMenu();
+
+
         public void ShowStat()
         {
-            Player player = new Player();
-            ConvertClassCode convertClassCode = new ConvertClassCode();
-
             string className = convertClassCode.ConvertClassCodeToString(player.ClassCode);
+            string goToMainMenu = "";
+            
+            do
+                
+            {
+                Console.Clear();
 
-            Console.WriteLine(" [상태보기] ");
-            Console.WriteLine("\n 캐릭터의 정보가 표시됩니다. ");
-            Console.WriteLine($"\n +=========================+");
-            Console.WriteLine($"\n Lv. {player.PlayerLevel}");
-            Console.WriteLine($" Chad ( {className} )");
-            Console.WriteLine($" 공격력 : {player.Attack}");
-            Console.WriteLine($" 방어력 : {player.Defense}");
-            Console.WriteLine($" 체 력 : {player.Health}");
-            Console.WriteLine($" Gold : {player.Gold}");
+                Console.WriteLine(" [상태보기] ");
+                Console.WriteLine("\n 캐릭터의 정보가 표시됩니다. ");
+                Console.WriteLine($"\n +=========================+");
+                Console.WriteLine($"\n Lv. {player.PlayerLevel}");
+                Console.WriteLine($" Chad ( {className} )");
+                Console.WriteLine($" 공격력 : {player.Attack}");
+                Console.WriteLine($" 방어력 : {player.Defense}");
+                Console.WriteLine($" 체 력 : {player.Health}");
+                Console.WriteLine($" Gold : {player.Gold}");
 
-            Console.WriteLine("\n1");
-            Console.WriteLine("0. 나가기");
+                Console.WriteLine("\n0. 나가기");
 
-            Console.WriteLine("\n원하시는 행동을 입력해주세요.");
+                
+               
+                goToMainMenu = Console.ReadLine();
 
+                switch (goToMainMenu)
+                {
+                    case "0":
+                        mainMenu.DisplayMainMenu();
+                        break;
 
+                    default:
+                        Console.WriteLine("잘못된 입력입니다. 확인 후 다시 입력해주세요.\n\n\n");
+                        Console.ReadKey();// 이건 잠깐 멈춰! 해주는거
+                        break;
+                }
 
-
+            } while (goToMainMenu != "0");
 
         }
     }

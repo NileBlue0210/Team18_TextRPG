715dd2abbea85181369a37efbeb02037897dbd90
Saint_Elion <pocklyuck@naver.com>
2025-04-22 21:17:10 +0900

    1. 세부사항 수정 2. 승리&패배에 따른 출력사항 수정 중간 저장
    # 승리, 패배 관련 함수는 Battle에서, 승리 조건 중 몬스터 == 0이 되는 부분은 Monster에서 확인할 것

--- Team18_TextRPG/Team18_TextRPG/EndStageView.cs
+++ Team18_TextRPG/Team18_TextRPG/EndStageView.cs
@@ -2,26 +2,36 @@
 
 namespace Sparta_Team18_TextRPG
 {
-    internal class EndStageView
+    public class EndStageView
     {
-        static void EndStage()
+        public void EndStage()
         {
+            Player player = new Player();
+            ConvertClassCode convertClassCode = new ConvertClassCode();
+            string result;
+
+            if (player.Health <= 0)  // 승리 & 패배
+            {
+                result = "패배...";
+            }
+            else if (player.Health > 0) // 플레이어 체력이 0보다 높을 때 && 몬스터 수가 0일 때
+            {
+                result = "승리!";
+            }
+
             while (true)
             {
-                Player player = new Player();
-                ConvertClassCode convertClassCode = new ConvertClassCode();
 
                 Console.WriteLine("배틀 결과\n");
-                Console.WriteLine(""); //승리 & 패배, 시간차 주기
+                Console.WriteLine($"{result}");
                 Console.WriteLine("system message\n");
-                Console.WriteLine("+==================+");
-
+                Console.WriteLine("+======================+");
                 Console.WriteLine("내 정보\n");
-                Console.WriteLine($"{player.Name}({convertClassCode.ConvertClassCodeToString(player.ClassCode)})");
-                Console.WriteLine($"체력: {player.Health}");
+                Console.WriteLine($"{player.Name} ({convertClassCode.ConvertClassCodeToString(player.ClassCode)})");
+                Console.WriteLine($"체력: {player.Health}");                                                                   // 시간차 구현(승리,패배 -> 바뀐 체력 -> 바뀐 골드)
                 Console.WriteLine($"골드: {player.Gold}\n\n");
                 Console.WriteLine("대상을 선택해주세요>>\n");
-                Console.WriteLine("전투옵션\n");
+                Console.WriteLine("-전투옵션-");
                 Console.WriteLine("0.다음");
 
                 string input = Console.ReadLine();

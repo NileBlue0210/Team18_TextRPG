6e7ad84ebf1bf5db88de3ebcc7222882e81f3563
NileBlue0210 <bjw8594@gmail.com>
2025-04-22 16:26:58 +0900

    the first commit

---
+++ Team18_TextRPG/Team18_TextRPG/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team18_TextRPG
+{
+    internal class MainMenu
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

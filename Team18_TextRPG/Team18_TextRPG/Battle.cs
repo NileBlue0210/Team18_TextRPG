bca90b37c60e2589dfcda5b62e4dc6bfa3d019a0
YeonDarm <msangyun29@naver.com>
2025-04-23 11:54:46 +0900

    전투 시스템 (디벨롭 작업용)

--- Team18_TextRPG/Team18_TextRPG/Battle.cs
+++ Team18_TextRPG/Team18_TextRPG/Battle.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Numerics;
+using System.Text;
 using System.Threading;
 using System.Xml.Linq;
 
@@ -10,20 +11,20 @@ namespace Sparta_Team18_TextRPG
         private static Random random = new Random();
         private static List<Monster> monsters = new List<Monster>();
 
-        private static Dictionary<string, (int minHealth, int maxHealth, int minAttack, int maxAttack)> monsterStats =
-        new Dictionary<string, (int, int, int, int)>
+        private static Dictionary<string, (int level, int minHealth, int maxHealth, int minAttack, int maxAttack)> monsterStats =
+        new Dictionary<string, (int, int, int, int, int)>
         {
-            { "  슬라임  ", (30, 50, 5, 15) },
-            { "  고블린  ", (40, 60, 10, 20) },
-            { "   좀비   ", (50, 70, 15, 25) },
-            { " 스켈레톤 ", (50, 80, 10, 17) },
-            { "   늑대   ", (60, 80, 20, 30) },
-            { "   거미   ", (70, 100, 15, 25) },
-            { "   트롤   ", (80, 120, 20, 35) },
-            { "   오크   ", (80, 130, 15, 25) },
-            { "  사냥꾼  ", (50, 80, 15, 25) },
-            { "  암살자  ", (50, 80, 15, 25) },
-            { " 흑마법사 ", (70, 110, 20, 30) },
+            { "  슬라임  ", (1, 10, 20, 3, 8) },
+            { "  고블린  ", (2, 15, 25, 10, 20) },
+            { "   좀비   ", (3, 16, 26, 3, 25) },
+            { " 스켈레톤 ", (5, 30, 45, 10, 12) },
+            { "   늑대   ", (7, 35, 60, 12, 15) },
+            { "   거미   ", (7, 20, 27, 3, 13) },
+            { "   트롤   ", (10, 80, 120, 20, 35) },
+            { "   오크   ", (15, 40, 50, 15, 25) },
+            { "  사냥꾼  ", (12, 35, 50, 22, 27) },
+            { "  암살자  ", (20, 23, 34, 12, 37) },
+            { " 흑마법사 ", (24, 17, 29, 33, 40) },
         };
 
         public static List<Monster> GetOrCreateMonsters()
@@ -38,10 +39,11 @@ namespace Sparta_Team18_TextRPG
                     string name = monsterNames[random.Next(monsterNames.Count)];
                     var stats = monsterStats[name];
 
+                    int level = random.Next(1, 3);
                     int health = random.Next(stats.minHealth, stats.maxHealth + 1);
                     int attack = random.Next(stats.minAttack, stats.maxAttack + 1);
 
-                    monsters.Add(new Monster(name, health, attack, MonsterStatus.Normal));
+                    monsters.Add(new Monster(level, name, health, attack, MonsterStatus.Normal));
                 }
             }
             return monsters;
@@ -55,12 +57,26 @@ namespace Sparta_Team18_TextRPG
 
     public class Battle
     {
+        StringBuilder sb = new StringBuilder();
+
         Player player = new Player();
         MainMenu mainMenu = new MainMenu();
         private List<Monster> monsters = new List<Monster>();
+        
 
         public void PlayerInfo()
         {
+            //sb.AppendLine("스트링빌더 적용 중");
+            //sb.AppendLine("\n+===================================+\n");
+            //sb.AppendLine("[내 정보]\n");
+            //sb.Append($"Lv. {player.PlayerLevel} ");
+            //sb.Append($"이름: {player.Name} ");
+            //sb.AppendLine($"({player.ClassCode})\n");
+            //sb.AppendLine($"체력: {player.Health}");
+            //sb.AppendLine("\n+-----------------------------------+\n");
+            //string result = sb.ToString();
+            //Console.WriteLine( result );
+            Console.WriteLine("\n");
             Console.WriteLine("\n+================================+\n");
             Console.WriteLine("[내 정보]\n");
             Console.Write($"Lv. {player.PlayerLevel} ");
@@ -76,7 +92,7 @@ namespace Sparta_Team18_TextRPG
             monsters = MonsterFactory.GetOrCreateMonsters();
             for (int i = 0; i < monsters.Count; i++)
             {
-                Console.WriteLine($"- {monsters[i].Name} | 체력: {monsters[i].Health} | 공격력: {monsters[i].Attack}");
+                Console.WriteLine($"- Lv.{monsters[i].Level} {monsters[i].Name} | 체력: {monsters[i].Health} | 공격력: {monsters[i].Attack}");
             }
             PlayerInfo();
             Console.WriteLine("전투 개시");
@@ -116,43 +132,83 @@ namespace Sparta_Team18_TextRPG
                 Console.Clear();
                 for (int i = 0; i < monsters.Count; i++)
                 {
-                    Console.WriteLine($"{i + 1}. {monsters[i].Name} | 체력: {monsters[i].Health} | 공격력: {monsters[i].Attack}");
+                    Console.WriteLine($"{i + 1}) Lv.{monsters[i].Level} {monsters[i].Name} | 체력: {monsters[i].Health} | 공격력: {monsters[i].Attack}");
                 }
                 PlayerInfo();
                 Console.WriteLine("공격할 몬스터를 선택하세요!");
-                Console.WriteLine("0.도망가기");
+                Console.WriteLine("0.페이즈 넘기기");
                 Console.Write(">> ");
 
                 string input = Console.ReadLine();
                 if (int.TryParse(input, out int monsterIndex) && monsterIndex >= 1 && monsterIndex <= monsters.Count)
                 {
-                    Monster target = monsters[monsterIndex - 1];
+                    case "0":
+                        //Console.Write("도망쳤습니다. 다시 마을로 돌아갑니다.");
+                        Console.Write("페이즈를 넘깁니다.");
+                        Console.ReadLine();
+                        MonsterAttack();
+                        break;
 
                     Console.WriteLine($"{target.Name}에게 공격!");
                     target.Health -= 10;
 
-                    if (target.Health <= 0)
-                    {
-                        Console.WriteLine($"{target.Name}을(를) 처치했다!");
-                        monsters.RemoveAt(monsterIndex - 1);
-                    }
-                }
+                            target.Health -= 100; // 전투 테스트 중
+                            Console.WriteLine($"{target.Name}에게 공격!");
+                            if (target.Health <= 0)
+                            {
+                                Console.WriteLine($"{target.Name}을(를) 처치했다!");
+                                monsters.RemoveAt(monsterIndex - 1);
+                            }
 
-                if (input == "0")
-                {
-                    Console.WriteLine("도망쳤습니다. 다시 마을로 돌아갑니다.");
-                    Console.ReadLine();
-                    return;
+                            MonsterAttack();
+
+                            if (player.Health <= 0) // 플레이어 
+                            {
+                                GameOver();
+                                return;
+                            }
+                        }
+                        break;
                 }
-                Console.WriteLine("\n계속 진행하려면 Enter를 누르세요.");
+            }
+            BattleEnd(); // 전투 종료되면 마을로 돌아가기 실행.
+        }
+        private void EnemyPhase()
+        {
+            
+        }
+        private void MonsterAttack()
+        {
+            Console.Clear();
+            Console.WriteLine($"\n몬스터들의 반격!\n");
+            foreach (Monster monster in monsters)
+            {
+                if (player.Health <= 0) break;
+                int damage = (int)monster.Attack;
+                player.Health -= damage;
+
+                Console.WriteLine($"{monster.Name} 공격! | -{damage} 피해");
+                Console.WriteLine($"플레이어 남은 체력: {player.Health}");
                 Console.ReadLine();
             }
+        }
 
+        private void BattleEnd()
+        {
+            //전투 결과 출력 구현부
             Console.WriteLine("모든 적을 처치했습니다! 마을로 돌아갑니다.");
             MonsterFactory.ClearMonsters(); //  전투 종료 후 리스트 비우기
+            Console.WriteLine("계속 하려면 Enter를 누르세요.");
+            Console.Write(">> ");
             Console.ReadLine();
         }
 
+        private void GameOver()
+        {
+            Console.WriteLine("플레이어가 쓰러졌습니다... 전투 종료");
+            monsters.Clear();
+            Console.ReadLine();
+        }
     }
 }
 
--- Team18_TextRPG/Team18_TextRPG/MainMenu.cs
+++ Team18_TextRPG/Team18_TextRPG/MainMenu.cs
@@ -9,18 +9,66 @@ namespace Sparta_Team18_TextRPG
     public class MainMenu
     {
 
-        public void Nickname()
+        //public void Nickname()
+        //{
+        //    Status status = new Status();//상태창 스크립트 연결 필요###########################################상태창 스크립트 클래스 이름 Status에 덮어씌우기
+        //    Battle battle = new Battle();//전투창 스크립트 연결 필요###########################################전투창 스크립트 클래스 이름 Battle에 덮어씌우기
+        //    Console.Clear();
+        //    string StartAnswer;
+
+        //    do
+        //    {
+
+        //        Console.Write(@"스파르타 던전에 오신 여러분 환영합니다.");
+        //        Console.Write("이제 전투를 시작할 수 있습니다.");
+
+        //        Console.WriteLine("1.상태보기");
+        //        Console.WriteLine("2.전투시작");
+
+        //        Console.WriteLine("원하시는 행동을 입력해주세요");
+
+        //        Console.WriteLine(">> ");
+
+        //        StartAnswer = Console.ReadLine();
+
+        //        switch (StartAnswer)
+        //        {
+        //            case "1":
+        //                //상태창 로직
+        //                status.ShowStat();//###########################################상태창 스크립트 함수 이름 DisplayStatusmenu에 덮어씌우기
+        //                break;
+        //            case "2":
+        //                // 전투시작 로직
+        //                battle.BattleScene();//###########################################전투창 스크립트 함수 이름 DisplayBattleMenu에 덮어씌우기
+        //                break;
+        //            default:
+        //                Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요.\n\n\n");
+        //                break;
+        //        }
+
+        //    } while (StartAnswer != "1" || StartAnswer != "2");
+        //}
+
+        public void DisplayMainMenu()
         {
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //    string CheckAnswer;
+            //    string NameAnswer;
+            //    Console.Write("스파르타 던전에 오신 여러분 환영합니다.\n");
+            //    Console.Write("원하시는 이름을 설정해 주세요.\n");
+            //    Console.Write(">>");
+            //    NameAnswer = Console.ReadLine();
+            //    Console.Write($"({NameAnswer}) 이 이름으로 하시겠습니까?\n\n1.확정\n2.취소 \n >>");
+            //    CheckAnswer = Console.ReadLine();
+            //}
             Status status = new Status();//상태창 스크립트 연결 필요###########################################상태창 스크립트 클래스 이름 Status에 덮어씌우기
             Battle battle = new Battle();//전투창 스크립트 연결 필요###########################################전투창 스크립트 클래스 이름 Battle에 덮어씌우기
             Console.Clear();
             string StartAnswer;
-
             do
             {
-
                 Console.Write(@"스파르타 던전에 오신 여러분 환영합니다.");
-                Console.Write("이제 전투를 시작할 수 있습니다.");
+                Console.WriteLine("이제 전투를 시작할 수 있습니다.");
 
                 Console.WriteLine("1.상태보기");
                 Console.WriteLine("2.전투시작");
@@ -39,7 +87,7 @@ namespace Sparta_Team18_TextRPG
                         break;
                     case "2":
                         // 전투시작 로직
-                        battle.BattleScene();//###########################################전투창 스크립트 함수 이름 DisplayBattleMenu에 덮어씌우기
+                        battle.BattleStart();//###########################################전투창 스크립트 함수 이름 DisplayBattleMenu에 덮어씌우기
                         break;
                     default:
                         Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요.\n\n\n");
@@ -47,21 +95,6 @@ namespace Sparta_Team18_TextRPG
                 }
 
             } while (StartAnswer != "1" || StartAnswer != "2");
-        }
-
-        public void DisplayMainMenu()
-        {
-            string CheckAnswer;
-            string NameAnswer;
-            Console.Write("스파르타 던전에 오신 여러분 환영합니다.\n");
-            Console.Write("원하시는 이름을 설정해 주세요.\n");
-            Console.Write(">>");
-            NameAnswer = Console.ReadLine();
-            Console.Write($"({NameAnswer}) 이 이름으로 하시겠습니까?\n\n1.확정\n2.취소 \n >>");
-            CheckAnswer = Console.ReadLine();
-            
-
-
         }
     }
 }
